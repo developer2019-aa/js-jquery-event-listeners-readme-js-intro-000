b776077a29de426f123b9db535c4be272467cc33
Allen <developer2019@icloud.com>
2020-05-13 13:16:00 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,10 +15,7 @@ function pressIt() {
 
 function submitIt() {
   $('form').on('submit', function () {
-    if ($('input:first').val() === 'correct') {
       alert ('Your form is going to be submitted now.')
-      return;
-    }
   })
 }
 
